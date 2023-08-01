20d79a941142171c78bf387b9c762fe9fe8c3045
anandlaggishetti0314 <136552213+anandlaggishetti0314@users.noreply.github.com>
2023-08-01 14:46:16 +0530

    Delete Backend-springBoot directory

--- Backend-springBoot/rfq-service/src/main/java/com/ojas/securisk/rfqservice/repository/IntermediateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ojas.securisk.rfqservice.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.ojas.securisk.rfqservice.Entity.IntermediateDetails;
-
-public interface IntermediateRepository extends MongoRepository<IntermediateDetails, String> {
-
-}
